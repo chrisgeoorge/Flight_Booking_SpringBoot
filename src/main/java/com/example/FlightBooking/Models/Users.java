1646b5cd38a38ddbc9fc0c7010986deaae68fa24
Sreya45 <sreyavijay5@gmail.com>
2025-02-28 10:15:24 +0530

    User Repository and User Model

--- src/main/java/com/example/FlightBooking/Models/Users.java
+++ src/main/java/com/example/FlightBooking/Models/Users.java
@@ -1,4 +1,80 @@
 package com.example.FlightBooking.Models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("age")
+    private int age;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("phone")
+    private String phone;
+
+    @JsonProperty("password")
+    private String password;
+
+    public Users() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String  getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/example/FlightBooking/Repository/UserRepository.java
+++ src/main/java/com/example/FlightBooking/Repository/UserRepository.java
@@ -1,4 +1,32 @@
 package com.example.FlightBooking.Repository;
 
-public interface UserRepository {
+import com.example.FlightBooking.Models.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import jakarta.transaction.Transactional;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<Users, Integer> {
+
+    @Query(value = "SELECT * FROM users WHERE email = ?1 AND password = ?2", nativeQuery = true)
+    List<Users> validation(String email, String password);
+
+    @Query(value = "SELECT * FROM users WHERE user_id = ?1", nativeQuery = true)
+    Users viewDetails(int userId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM users WHERE user_id = ?1", nativeQuery = true)
+    void deleteUser(int userId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE users SET phone_no = ?2 WHERE user_id = ?1", nativeQuery = true)
+    void updatePhoneNo(int userId, String phoneNo);
+
+    /*@Query(value = "SELECT u.user_id, u.name, u.email, u.phone_no, b.booking_id, b.flight_id, b.status " +
+            "FROM users u LEFT JOIN bookings b ON u.user_id = b.user_id", nativeQuery = true)
+    List<Object[]> getAllUsersWithBookings();*/
 }
